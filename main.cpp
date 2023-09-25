91b663f53e72fc9e3c7c09ae3c60386f500784ea
Galax <Galax@GORD5446>
2023-09-25 17:46:16 -0400

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+/*****************************************************************************
+*                     CSCN72030 F23 / Project III                            *
+*                     Professor:  Ahmed Salamah                              *
+*                                                                            *
+*	              BY:  Sudhan Dahake - Hangsihak Sin - Sierra Erb -          *
+*                      Michelle Novar - Saba Berenji                         *
+*               DATE:  September, 2023                                       *
+*        DESCRIPTION:  Project III - main.cpp file                           *
+*                                                                            *
+*                                                                            *
+******************************************************************************/
+
+int main()
+{
+	cout << "Hello World!!!" << endl;
+
+	return 0;
+}
