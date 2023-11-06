ef8bf026029e0dc157e36305c6699094026de0dd
Michelle Gordon <Mgordon9073@conestogac.on.ca>
2023-11-06 18:29:20 -0500

    updated

--- inputmodule.cpp
+++ inputmodule.cpp
@@ -35,31 +35,3 @@ public:
         return password;                        // returns string password
     }
 };
-
-//
-//
-//#include <iostream>
-//using namespace std;
-//
-//class Employee {
-//private:
-//    // Private attribute
-//    int salary;
-//
-//public:
-//    // Setter
-//    void setSalary(int s) {
-//        salary = s;
-//    }
-//    // Getter
-//    int getSalary() {
-//        return salary;
-//    }
-//};
-//
-//int main() {
-//    Employee myObj;
-//    myObj.setSalary(50000);
-//    cout << myObj.getSalary();
-//    return 0;
-//}
