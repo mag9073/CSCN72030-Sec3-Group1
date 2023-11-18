5f2b16ce467327c4b54e8842097f1c1bbcb8fa58
Saba <sberenji4567@conestogac.on.ca>
2023-11-18 12:24:36 -0500

    Added pyqt Settings class to main.py for record

--- python_modules/main.py
+++ python_modules/main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt6 import QtWidgets, QtGui
-from PyQt6.QtWidgets import QDialog, QLineEdit, QVBoxLayout
+from PyQt6.QtWidgets import QDialog, QLineEdit, QVBoxLayout, QLabel
 from PyQt6.uic import loadUi
 from PyQt6.QtGui import QFont
 from PyQt6.QtCore import Qt
@@ -17,13 +17,13 @@ DESCRIPTION: Project III - main.py file
 '''
 
 
-# First Screen - Login Screen - class Inheriting from QDialog class. 
+# First Screen - Login Screen - class Inheriting from QDialog class.
 class LoginScreen(QDialog):
     def __init__(self, app):
         super(LoginScreen, self).__init__()
 
         # Load login interface
-        loadUi("./interfaces/login.ui", self)    
+        loadUi("./interfaces/login.ui", self)
 
         # Click functions when pressing buttons.
         self.passwordfield.setEchoMode(QLineEdit.EchoMode.Password)
@@ -36,7 +36,6 @@ class LoginScreen(QDialog):
         self.expected_username = "hsihak"
         self.expected_password = "123456789"
 
-    
     def loginButtonClicked(self):
 
         # Get entered user info
@@ -46,7 +45,7 @@ class LoginScreen(QDialog):
         # Display an error message if user attempts to click on Login button while the field is empty
         if len(username) == 0 or len(password) == 0:
             self.error.setText("Please input all fields.")
-        
+
         # This statement autheticate username and password whether the user exists.
             # Actual should be calling authetication backend module to verify
         else:
@@ -66,6 +65,8 @@ class LoginScreen(QDialog):
                 self.error.setText("Invalid username or password")
 
 # Second Screen - Dashboard Screen - class inheriting from QDialog class
+
+
 class DashboardScreen(QDialog):
     def __init__(self, app):
         super(DashboardScreen, self).__init__()
@@ -79,11 +80,11 @@ class DashboardScreen(QDialog):
         # Store the app instance for later use
         self.app = app
 
-
     def settingsButtonClicked(self):
 
         # Create Settings Screen instance
-        settingsScreen = SettingsScreen(self.app, dashboard_bgwidget=self.parentWidget())
+        settingsScreen = SettingsScreen(
+            self.app, dashboard_bgwidget=self.parentWidget())
 
         print(self.parentWidget())
 
@@ -91,9 +92,75 @@ class DashboardScreen(QDialog):
         widget.addWidget(settingsScreen)
 
         # Advance to the next screen (settings screen)
-        widget.setCurrentIndex(widget.currentIndex() + 1)              
+        widget.setCurrentIndex(widget.currentIndex() + 1)
 
 
+"""
+The following code is the Settings class by Saba Berenji which was initially going to be used for the implementation of the 
+Settings interface but will not be used anymore due to the change of frontend framework from PyQy to React
+"""
+
+
+class SettingsScreen(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        loadUi("settings.ui", self)
+        self.connectingToFunctions()
+
+    def connectingToFunctions(self):
+        # connecting the LightModeButton button to its associated light_mode function
+        self.LightModeButton.toggled.connect(self.light_mode)
+        # connecting the DarkModeButton button to its associated dark_mode function
+        self.DarkModeButton.toggled.connect(self.dark_mode)
+        # Return back to Main Page
+        self.backButton.clicked.connect(self.back_to_main)
+
+        self.spinBox.valueChanged.connect(self.change_font_size)
+
+    def light_mode(self):
+        # checking to see if the LightModeButton is selected or not. If selected, the stylesheet will be set to the arguments passed to it
+        if self.LightModeButton.isChecked():
+            self.bgwidget.setStyleSheet("QWidget {background-color: #D4D6CF;}"
+                                        "QRadioButton { color: #000000; } "
+                                        )
+
+            # setting the color of the labels in the light mode to black
+            for label in self.bgwidget.findChildren(QLabel):
+                label.setStyleSheet("color: #000000;")
+            # this line is to set the size of the greet_message_3 label to 16 in order to avoid the change of font size when changing modes
+            self.greet_message_3.setStyleSheet(
+                "font: 16pt 'MS Shell Dlg 2'; color: #000000")
+
+    def dark_mode(self):
+        # checking to see if the DarkModeButton is selected or not. If selected, the stylesheet will be set to the arguments passed to it
+        if self.DarkModeButton.isChecked():
+            self.bgwidget.setStyleSheet("QWidget {background-color: #333333;}"
+                                        "QRadioButton { color: #ffffff; } "
+                                        "QPushButton { color: #ffffff; }")
+
+            for label in self.bgwidget.findChildren(QLabel):
+                label.setStyleSheet("color: #FFFFFF")
+            # this line is to set the size of the greet_message_3 label to 16 in order to avoid the change of font size when changing modes
+            self.greet_message_3.setStyleSheet(
+                "font: 16pt; color: #FFFFFF")
+
+    def back_to_main(self):
+
+        widget.setCurrentIndex(widget.currentIndex() - 1)
+
+    size = max(12, 36)
+
+    def change_font_size(self, size):
+        # set the range of the font size from 12 to 36
+
+        for label in self.bgwidget.findChildren(QLabel):
+            font = QFont("MS Shell Dlg 2")
+            font.setPointSize(size)
+            label.setFont(font)
+
+
+"""
 # Second subscreen - Settings - class inheriting from QDialog class
 class SettingsScreen(QDialog):
     def __init__(self, app, dashboard_bgwidget):
@@ -103,12 +170,13 @@ class SettingsScreen(QDialog):
 
         self.app = app
 
-        self.selected_color = "default" 
+        self.selected_color = "default"
 
         self.setting_bgwidget = self.color_dropmenu.parentWidget()
         self.dashboard_bgwidget = dashboard_bgwidget
 
-        self.back_button.clicked.connect(self.backToMain)   # Return back to Main Page
+        self.back_button.clicked.connect(
+            self.backToMain)   # Return back to Main Page
         # self.color_dropmenu.currentTextChanged.connect(self.changeBackgroundColor)
 
         # # Add colors to dropdown menu for background change
@@ -141,8 +209,6 @@ class SettingsScreen(QDialog):
     # Connect the toggle to the background color change
         self.toggle.stateChanged.connect(self.handleToggleState)
 
-
-
     def backToMain(self):
         widget.setCurrentIndex(widget.currentIndex() - 1)
 
@@ -157,7 +223,7 @@ class SettingsScreen(QDialog):
     #         self.change("pink")
 
     def change(self, color):
-        
+
         # Change background color to different widgets
         self.setting_bgwidget.setStyleSheet(f"background-color: {color};")
         self.dashboard_bgwidget.setStyleSheet(f"background-color: {color};")
@@ -165,7 +231,7 @@ class SettingsScreen(QDialog):
 
     def sliderValueChanged(self):
         slider_value = self.fontsize_adjuster.value()
-        
+
         # Map slider values to positions
         position_mapping = {
             0: 10,
@@ -184,7 +250,6 @@ class SettingsScreen(QDialog):
             current_widget = widget.widget(index)
             for label in current_widget.findChildren(QtWidgets.QLabel):
                 label.setFont(font)
-
 
     # def toggleBackgroundColor(self):
     #     if self.toggle.isChecked():
@@ -207,9 +272,11 @@ class SettingsScreen(QDialog):
 
             # Change the text color to white for the background widget
             self.setting_bgwidget.setStyleSheet("color: #FFFFFF;")
-            label_palette.setColor(QtGui.QPalette.ColorRole.WindowText, QtGui.QColor("white"))
+            label_palette.setColor(
+                QtGui.QPalette.ColorRole.WindowText, QtGui.QColor("white"))
         else:  # Toggle is unchecked
-            self.change("#D4D6CF")  # Change background color back to the default
+            # Change background color back to the default
+            self.change("#D4D6CF")
 
             # Change the text color to the original color for the background widget
             self.setting_bgwidget.setStyleSheet("color: #000000;")
@@ -220,11 +287,13 @@ class SettingsScreen(QDialog):
             current_widget = widget.widget(index)
             for label in current_widget.findChildren(QtWidgets.QLabel):
                 current_style = label.styleSheet()
-                font_size_match = re.search(r'font-size:\s*(\d+)\s*px', current_style)
+                font_size_match = re.search(
+                    r'font-size:\s*(\d+)\s*px', current_style)
                 if font_size_match:
                     font_size = font_size_match.group(1)
-                    label.setStyleSheet(f"font-size: {font_size}px; {label_style}")
-
+                    label.setStyleSheet(
+                        f"font-size: {font_size}px; {label_style}")
+"""
 
 # Encapsulate all the code to only run in the main program
 if __name__ == "__main__":
@@ -246,7 +315,7 @@ if __name__ == "__main__":
     # Add login screen to the widget stack
     widget.addWidget(login)
 
-    # Set the app widget fixed height and width 
+    # Set the app widget fixed height and width
     widget.setFixedHeight(800)
     widget.setFixedWidth(1200)
 
@@ -255,10 +324,9 @@ if __name__ == "__main__":
 
     # Create SettingScreen instance which takes app and dahsboard_bgwidget as its arguments
     setting_screen = SettingsScreen(app, dashboard_bgwidget=dashboard.bgwidget)
-    
+
     # Error handlings
     try:
         sys.exit(app.exec())
     except SystemExit:
         print('Closing Window...')
-
