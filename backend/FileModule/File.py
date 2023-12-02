9e12b8d9ac49b3dfcba1fdd0376fabe4220c1e82
serb2341 <113473037+serb2341@users.noreply.github.com>
2023-12-01 19:04:42 -0500

    Delete backend/FileModule/File.py

--- backend/FileModule/File.py
+++
@@ -1,9 +0,0 @@
-class File():
-    def __init__(self, filename):
-        self.__filename = filename
-
-    def setFilename(self, filename):
-        self.__filename = filename
-
-    def getFilename(self):
-        return self.__filename
