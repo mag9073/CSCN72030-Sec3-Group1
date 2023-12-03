b7b9a71311bd2b215e2a07b4dcbae6ec605742a7
Sudhan <sudhandahake12@gmail.com>
2023-12-03 00:35:57 -0500

    pulled integration branch and implemented KNN

--- backend/KNNModule/KNNFiles/KNNFileOperations.py
+++ backend/KNNModule/KNNFiles/KNNFileOperations.py
@@ -1,22 +1,14 @@
 import pandas as pd
 
-class SavingPredictedResults:
-    def __init__(self, fileName, predictedResult):
-        self.__fileName = fileName
-        self.__predictedResult = predictedResult
 
-    def saveToFile(self):
-        with open(self.__fileName, "w") as my_file:
-            for element in self.__predictedResult:
-                my_file.write(f"{element}")
-
-
-
-class ReadingPatientFile:
-    def __init__(self, fileName):
-        self.__fileName = fileName
+class DiseaseFile():
+    def saveToFile(self, fileName, dataframe):
+        dataframe.to_csv(fileName, index=False)
+        
 
     def readFromFile(self):
-        dataframe = pd.read_csv(self.__fileName)
+        fileName = "PatientData.csv"
 
-        return dataframe[dataframe.columns[ : ]].values
+        dataframe = pd.read_csv(fileName)
+
+        return dataframe
